6e5d41df1dd0a1571699430728c321357530cdaf
Subhashini R <91070669+Subha2123@users.noreply.github.com>
2022-07-31 20:31:09 +0530

    Add files via upload

---
+++ single-page/src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <a href="#about">About&nbsp;&nbsp;&nbsp;</a>
+        <a href="#content">Content&nbsp;&nbsp;&nbsp;</a>
+        <a href="#more">More</a>
+      </div>
+      <h1 id="about">About</h1>
+      <p>
+        React is a free and open-source front-end JavaScript library for
+        building user interfaces based on UI components. It is maintained by
+        Meta and a community of individual developers and companies. React is a
+        free and open-source front-end JavaScript library for building user
+        interfaces based on UI components. It is maintained by Meta and a
+        community of individual developers and companies. React is a free and
+        open-source front-end JavaScript library for building user interfaces
+        based on UI components. It is maintained by Meta and a community of
+        individual developers and companies. React is a free and open-source
+        front-end JavaScript library for building user interfaces based on UI
+        components. It is maintained by Meta and a community of individual
+        developers and companies.
+      </p>
+
+      <h1 id="content">Content</h1>
+      <p>
+        applications that can change data, without reloading the page. The main
+        purpose of React is to be fast, scalable, and simple. It works only on
+        user interfaces in the application. This corresponds to the view in the
+        MVC template applications that can change data, without reloading the
+        page.
+      </p>
+
+      <h1 id="more">More</h1>
+      <p>
+        The main purpose of React is to be fast, scalable, and simple. It works
+        only on user interfaces in the application. This corresponds to the view
+        in the MVC template applications that can change data, without reloading
+        the page. The main purpose of React is to be fast, scalable, and simple.
+        It works only on user interfaces in the application. This corresponds to
+        the view in the MVC template
+      </p>
+    </>
+  );
+}
